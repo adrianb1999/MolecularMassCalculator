c3c7bd11cf40c29012b52da534dc529f3a29d1a4
adrianb1999 <blanariu_adrian@yahoo.com>
2022-03-01 12:35:16 +0200

    Front-end added

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,40 @@
+function calculateMass() {
+
+    let formula = document.getElementById("formulaInput").value
+    let result = document.getElementById("massResult")
+    result.innerHTML = ""
+
+    fetch(`/api/massCalculator?formula=${formula}`,
+        {
+            method: 'GET',
+        })
+        .then(response => {
+                if (response.status === 200) {
+                    response.json().then(data => {
+
+                            for (i of data) {
+                                result.insertAdjacentHTML("beforeend",
+                                    `${i.element} : ${i.mass}<br>`
+                                )
+                            }
+                        }
+                    )
+                }
+                else {
+                    response.json().then(data => {
+                        result.insertAdjacentHTML("beforeend",data.message)
+                    })
+                }
+            }
+        )
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#formulaForm").onsubmit = function () {
+        calculateMass()
+        return false
+    }
+})

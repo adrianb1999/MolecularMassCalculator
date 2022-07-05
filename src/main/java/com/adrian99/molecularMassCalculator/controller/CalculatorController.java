59be250564e42ca28949b129ff4b1d66ed0f6bbf
adrianb1999 <blanariu_adrian@yahoo.com>
2022-07-05 17:17:44 +0300

    Controller refactoring

--- src/main/java/com/adrian99/molecularMassCalculator/controller/CalculatorController.java
+++ src/main/java/com/adrian99/molecularMassCalculator/controller/CalculatorController.java
@@ -1,6 +1,8 @@
 package com.adrian99.molecularMassCalculator.controller;
 
 import com.adrian99.molecularMassCalculator.calculator.MassCalculator;
+import com.adrian99.molecularMassCalculator.dto.FormulaDto;
+import com.adrian99.molecularMassCalculator.exception.InvalidFormulaException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,7 +18,11 @@ public class CalculatorController {
     }
 
     @GetMapping("/api/massCalculator")
-    public List<Map<String, Object>> calculator(@RequestParam(name = "formula") String molecularFormula) {
+    public Map<String, List<FormulaDto>> calculator(@RequestBody(required=false) Map<String, String> formula) {
+        if(formula == null || formula.isEmpty() || !formula.containsKey("formula") || formula.get("formula").isEmpty() || formula.get("formula").isBlank())
+            throw new InvalidFormulaException("Invalid formula!");
+
+        String molecularFormula = formula.get("formula");
 
         return massCalculator.startCalculator(molecularFormula);
     }
